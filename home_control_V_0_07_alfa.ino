dac86e982f2cb009e630b9b2a834176cadcb1a50
gmroczkowski <grzegorz.mroczkowski@gmail.com>
2020-05-17 17:08:41 +0200

    Change IP paramenters to test env

--- home_control_V_0_07_alfa.ino
+++ home_control_V_0_07_alfa.ino
@@ -155,8 +155,8 @@ const long timeoutTime = 2000;
 // Enter a MAC address and IP address for your controller below.
 // The IP address will be dependent on your local network:
 byte mac[] = {
-    0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
-IPAddress ip(172, 26, 160, 17);
+    0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xE8};
+IPAddress ip(172, 26, 160, 16);
 
 // Initialize the Ethernet server library
 // with the IP address and port you want to use
